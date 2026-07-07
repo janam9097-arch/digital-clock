f3fad97ab0332e5de0766e924a9a2c2a64082304
janam9097-arch <janam9097@gmail.com>
2026-07-07 20:57:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function updateclock(){
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2 , 0);
+  const minutes = now.getMinutes().toString().padStart(2 , 0);
+  const seconds = now.getSeconds().toString().padStart(2 , 0);
+  const timeSting = `${hours}:${minutes}:${seconds}`
+  document.getElementById("clock").textContent=timeSting;
+  
+}
+updateclock();
+setInterval(updateclock,1000)
